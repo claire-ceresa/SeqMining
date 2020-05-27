7c4243165ccc83f90c67a32675605b4f09cfea3f
claire-ceresa <40687281+claire-ceresa@users.noreply.github.com>
2020-05-27 19:31:49 +0200

    Implémentation de la requete multiple sur mongoDB

--- controllers/DB_Search_Window.py
+++ controllers/DB_Search_Window.py
@@ -4,6 +4,7 @@ from views.db_search_view import Ui_DB_Search
 import re
 import datetime
 
+
 class DB_Search_Window(QtWidgets.QMainWindow, Ui_DB_Search):
     """
     controlling class for principal_view
@@ -24,14 +25,9 @@ class DB_Search_Window(QtWidgets.QMainWindow, Ui_DB_Search):
         self.edit_download_2.hide()
 
     def button_search_clicked(self):
-        try:
-            query = self.construct_query()
-        except Exception as e:
-            print(e)
-        else:
-            print(query)
+        query = self.construct_query()
         results = self.mongoDB_connexion.collection.find(query)
-        self.window_result = DB_Result_Window(results = results)
+        self.window_result = DB_Result_Window(results=results)
         self.window_result.show()
 
     def combobox_date_changed(self, text):
@@ -52,10 +48,10 @@ class DB_Search_Window(QtWidgets.QMainWindow, Ui_DB_Search):
 
     def get_checkbox_corresp_key(self):
         checkboxes = {
-            'checkbox_id':'id',
-            'checkbox_descr':'description',
-            'checkbox_download':'download_date',
-            'checkbox_type':'annotations.molecule_type',
+            'checkbox_id': 'id',
+            'checkbox_descr': 'description',
+            'checkbox_download': 'download_date',
+            'checkbox_type': 'annotations.molecule_type',
             'checkbox_topo': 'annotations.topology',
             'checkbox_date': 'annotations.date',
             'checkbox_keyword': 'annotations.keywords',
@@ -83,35 +79,48 @@ class DB_Search_Window(QtWidgets.QMainWindow, Ui_DB_Search):
         return combobox_name
 
     def construct_query(self):
-        all_checkboxes = self.get_checkbox_corresp_key()
-        for checkbox_name in all_checkboxes:
-            checkbox = getattr(self, checkbox_name)
-            key = all_checkboxes[checkbox_name]
-            if checkbox.isChecked():
-                edit_name = self.find_associated_edit_name(checkbox)
-                if checkbox_name is 'checkbox_date' or checkbox_name is 'checkbox_download':
-                    combobox_name = self.find_associated_combobox_name(checkbox)
-                    combobox = getattr(self, combobox_name)
-                    operation = combobox.currentText()
-                    edit_1 = getattr(self, edit_name + "_1")
-                    text_1 = edit_1.text()
-                    date_1 = datetime.datetime.strptime(text_1, '%d/%m/%Y')
-                    if operation == 'avant':
-                        value = {"$lte":date_1}
-                    elif operation == 'apres':
-                        value = {'$gte':date_1}
-                    else:
-                        edit_2 = getattr(self, edit_name + "_2")
-                        text_2 = edit_2.text()
-                        date_2 = datetime.datetime.strptime(text_2, '%d/%m/%Y')
-                        value = {"$lte":date_1, "$gt":date_2}
-
+        checked_checkboxes = self.get_checked_checkboxes()
+        parts_of_query = []
+        for key, checkbox in checked_checkboxes.items():
+            checkbox_name = checkbox.objectName()
+            edit_name = self.find_associated_edit_name(checkbox)
+            if checkbox_name == 'checkbox_date' or checkbox_name == 'checkbox_download':
+                combobox_name = self.find_associated_combobox_name(checkbox)
+                combobox = getattr(self, combobox_name)
+                operation = combobox.currentText()
+                edit_1 = getattr(self, edit_name + "_1")
+                text_1 = edit_1.text()
+                date_1 = datetime.datetime.strptime(text_1, '%d/%m/%Y')
+                if operation == 'avant':
+                    value = {"$lte": date_1}
+                elif operation == 'apres':
+                    value = {'$gte': date_1}
                 else:
-                    edit = getattr(self, edit_name)
-                    text = edit.text()
-                    if checkbox_name is 'checkbox_species' or checkbox_name is 'checkbox_taxo':
-                        value = text.capitalize()
-                    else:
-                        value = re.compile(text, re.IGNORECASE)
-                query = {key:value}
-                return query
+                    edit_2 = getattr(self, edit_name + "_2")
+                    text_2 = edit_2.text()
+                    date_2 = datetime.datetime.strptime(text_2, '%d/%m/%Y')
+                    value = {"$lte": date_1, "$gt": date_2}
+            else:
+                edit = getattr(self, edit_name)
+                text = edit.text()
+                if checkbox_name == 'checkbox_species' or checkbox_name == 'checkbox_taxo':
+                    value = text.capitalize()
+                else:
+                    value = re.compile(text, re.IGNORECASE)
+            parts_of_query.append({key: value})
+
+        if len(parts_of_query) > 1:
+            query = {"$and":parts_of_query}
+        else:
+            query = parts_of_query[0]
+        return query
+
+    def get_checked_checkboxes(self):
+        checkbox_names = self.get_checkbox_corresp_key()
+        checked = {}
+        for checkbox_name in checkbox_names:
+            checkbox_widget = getattr(self, checkbox_name)
+            if checkbox_widget.isChecked():
+                key = checkbox_names[checkbox_name]
+                checked[key] = checkbox_widget
+        return checked
