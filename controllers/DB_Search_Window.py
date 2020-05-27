5cc0f5d245ac90d3b3e413ca662cbef6017d15c4
claire-ceresa <40687281+claire-ceresa@users.noreply.github.com>
2020-05-27 17:49:28 +0200

    Fonctionnement de la rechercher simple

--- controllers/DB_Search_Window.py
+++ controllers/DB_Search_Window.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtWidgets
 from views.db_search_view import Ui_DB_Search
+import re
+import datetime
 
 class DB_Search_Window(QtWidgets.QMainWindow, Ui_DB_Search):
     """
@@ -20,7 +22,15 @@ class DB_Search_Window(QtWidgets.QMainWindow, Ui_DB_Search):
         self.edit_download_2.hide()
 
     def button_search_clicked(self):
-        print(self.edit_download_1.text())
+        try:
+            query = self.construct_query()
+        except Exception as e:
+            print(e)
+        else:
+            print(query)
+        results = self.mongoDB_connexion.collection.find(query)
+        for result in results:
+            print(result)
 
     def combobox_date_changed(self, text):
         if text == "entre":
@@ -38,3 +48,68 @@ class DB_Search_Window(QtWidgets.QMainWindow, Ui_DB_Search):
             self.label_download_et.hide()
             self.edit_download_2.hide()
 
+    def get_checkbox_corresp_key(self):
+        checkboxes = {
+            'checkbox_id':'id',
+            'checkbox_descr':'description',
+            'checkbox_download':'download_date',
+            'checkbox_type':'annotations.molecule_type',
+            'checkbox_topo': 'annotations.topology',
+            'checkbox_date': 'annotations.date',
+            'checkbox_keyword': 'annotations.keywords',
+            'checkbox_comment': 'annotations.comment',
+            'checkbox_species': 'annotations.organism',
+            'checkbox_taxo': 'annotations.taxonomy',
+            'checkbox_title': 'annotations.references.title',
+            'checkbox_author': 'annotations.references.authors',
+            'checkbox_journal': 'annotations.references.journal'
+        }
+        return checkboxes
+
+    def find_associated_edit_name(self, checkbox):
+        name = checkbox.objectName()
+        name_split = name.split("_")
+        attribute = name_split[1]
+        edit_name = "edit_" + attribute
+        return edit_name
+
+    def find_associated_combobox_name(self, checkbox):
+        name = checkbox.objectName()
+        name_split = name.split("_")
+        attribute = name_split[1]
+        combobox_name = "combobox_" + attribute
+        return combobox_name
+
+    def construct_query(self):
+        all_checkboxes = self.get_checkbox_corresp_key()
+        for checkbox_name in all_checkboxes:
+            checkbox = getattr(self, checkbox_name)
+            key = all_checkboxes[checkbox_name]
+            if checkbox.isChecked():
+                edit_name = self.find_associated_edit_name(checkbox)
+                if checkbox_name is 'checkbox_date' or checkbox_name is 'checkbox_download':
+                    combobox_name = self.find_associated_combobox_name(checkbox)
+                    combobox = getattr(self, combobox_name)
+                    operation = combobox.currentText()
+                    edit_1 = getattr(self, edit_name + "_1")
+                    text_1 = edit_1.text()
+                    date_1 = datetime.datetime.strptime(text_1, '%d/%m/%Y')
+                    if operation == 'avant':
+                        value = {"$lte":date_1}
+                    elif operation == 'apres':
+                        value = {'$gte':date_1}
+                    else:
+                        edit_2 = getattr(self, edit_name + "_2")
+                        text_2 = edit_2.text()
+                        date_2 = datetime.datetime.strptime(text_2, '%d/%m/%Y')
+                        value = {"$lte":date_1, "$gt":date_2}
+
+                else:
+                    edit = getattr(self, edit_name)
+                    text = edit.text()
+                    if checkbox_name is 'checkbox_species' or checkbox_name is 'checkbox_taxo':
+                        value = text.capitalize()
+                    else:
+                        value = re.compile(text, re.IGNORECASE)
+                query = {key:value}
+                return query
